e23d8fbbb49ec2f2fdb46a40d9e330435865f1d7
amably <alex.mably@gmail.com>
2019-09-20 16:50:30 -0500

    edits to integrate with ensemble forest

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='Interpretability',
+setup(name='ShapExplainers',
     version='1.0.0',
     description='Wrappers for shapley value explanations for model interpretability',
-    packages=['Interpretability'],
+    packages=['ShapExplainers'],
     install_requires=["typing",
-                      "pandas"
+                      "pandas",
                       "numpy == 1.15.4",
-                      'scikit-learn == 0.20.3',
+                      'scikit-learn',
                       "shap == 0.29.3",
                       ],
 )

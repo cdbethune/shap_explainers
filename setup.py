1010e2fcd6206c23e6bb7e78905f2e345277afc0
Chris Bethune <cbethune@uncharted.software>
2020-05-29 09:35:33 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='ShapExplainers',
-    version='1.0.0',
+    version='1.0.2',
     description='Wrappers for shapley value explanations for model interpretability',
     packages=['ShapExplainers'],
     install_requires=["typing",
                       "pandas",
-                      "numpy == 1.17.3",
+                      "numpy == 1.18.2",
                       'scikit-learn',
                       "shap == 0.29.3",
                       ],
